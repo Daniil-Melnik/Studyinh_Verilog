cf2e5fce39e075865c56b7089acd92d49e048447
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-02-20 10:44:21 +0300

    working version with two initials

--- test_top_tb.sv
+++ test_top_tb.sv
@@ -14,15 +14,37 @@ test_top test_top_dut(
 
 initial
 begin
-  $display ("Sim Started!");
+  $display ("Sim A Started!");
+  $display ("0 - A = ", a_s);
+
   #1 a_s = 1'b0;
-  b_s = 1'b1;
-  #1 a_s = 1'b1;
-  b_s = 1'b0;
+  $display ("1 - A = ", a_s);
 
   #1 a_s = 1'b1;
-  b_s = 1'b1;
+  $display ("2 - A = ", a_s);
+
+  #1 a_s = 1'b1;
+  $display ("3 - A = ", a_s);
+
   $display ("Sim A Finish!");
+  #1 $stop;
+end
+
+initial
+begin
+  $display ("Sim B Started!");
+  $display ("0 - B = ", b_s);
+
+  #1 b_s = 1'b1;
+  $display ("1 - B = ", b_s);
+
+  #1 b_s = 1'b0;
+  $display ("2 - B = ", b_s);
+
+  #1 b_s = 1'b1;
+  $display ("3 - B = ", b_s);
+
+  $display ("Sim B Finish!");
   #1 $stop;
 end
 endmodule
